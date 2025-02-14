54e6e8f645b851a87bddb1f90e07bc324e3763d8
DeniWeb <98588545+DeniWeb@users.noreply.github.com>
2025-02-14 09:07:31 +0000

    Deploying to gh-pages from @ DeniWeb/goit-js-hw-11@7fc421cbf547a338d55a74ab304085973c79387b 🚀

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-/* empty css                      */(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import{a as m,i as l,S as u}from"./assets/vendor-CUAzmrUA.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const d="https://pixabay.com/api/",g="48809440-7649b5744f5d98f4a92a91a9c",p=15;function f(r){const i={key:g,q:r,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:p};return m.get(d,{params:i}).then(o=>o.data.hits.length===0?(iziToast.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}),[]):o.data.hits).catch(o=>(iziToast.error({title:"Error",message:"Something went wrong. Please try again later!",position:"topRight"}),[]))}function h(r){const{webformatURL:i,largeImageURL:o,tags:n,likes:e,views:t,comments:a,downloads:c}=r;return`<li class="gallery-item">
+                <a href="${o}" class="gallery-link">
+                    <img src="${i}" alt="${n}" class="gallery-image" />
+                </a>
+                <div class="desc">
+                    <ul class="desc-wraper">
+                        <li class="desc-item">
+                            <h3>Likes</h3>
+                            <p>${e}</p>
+                        </li>
+                        <li class="desc-item">
+                            <h3>Views</h3>
+                            <p>${t}</p>
+                        </li>
+                        <li class="desc-item">
+                            <h3>Comments</h3>
+                            <p>${a}</p>
+                        </li>
+                        <li class="desc-item">
+                            <h3>Downloads</h3>
+                            <p>${c}</p>
+                        </li>
+                    </ul>
+                </div>
+            </li>`}function y(r){return r.map(h).join("")}const s={container:document.querySelector(".gallery"),form:document.querySelector(".form"),loader:document.querySelector(".loader")};s.form.addEventListener("submit",r=>{r.preventDefault();const i=r.target.elements.text.value.trim();if(!i){l.error({title:"Error",message:"Something went wrong. Please try again later!",position:"topRight"});return}s.container.innerHTML="",s.loader.classList.remove("hidden"),f(i).then(o=>{L(o)}).catch(o=>{l.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"})}).finally(()=>{s.loader.classList.add("hidden"),r.target.reset()})});function L(r){const i=y(r);s.container.insertAdjacentHTML("beforeend",i),w.refresh()}const w=new u(".gallery a",{captionsData:"alt",captionDelay:250});
 //# sourceMappingURL=index.js.map
